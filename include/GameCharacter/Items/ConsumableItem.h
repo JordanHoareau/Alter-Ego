8349b7ffb0242f024d8e8cd2adad87ea0e2c989f
Jordan HOAREAU <j.hoareau2908@gmail.com>
2016-03-17 11:23:47 +0100

    ConsumableItem - updated
    # consumableitem
    - JSONable

--- include/GameCharacter/Items/ConsumableItem.h
+++ include/GameCharacter/Items/ConsumableItem.h
@@ -5,10 +5,27 @@
 
 class ConsumableItem : public SellableItem {
     public:
-        ConsumableItem(int id, std::string name, std::string description, int sellvalue, int buyvalue, int healthbonus)
-            : SellableItem(id){
-            m_healthbonus = healthbonus;
-            }
+        ConsumableItem(int id){
+            Json::Value root;
+            Json::Reader reader;
+            std::ifstream consumableitems_file("data\\Items\\ConsumableItems.json", std::ifstream::binary);
+            bool AttackparsingSuccessful = reader.parse( consumableitems_file, root );
+            if(AttackparsingSuccessful){
+                const Json::Value consumableitems_node = root["consumableitems"];
+                for(unsigned int i = 0; i < consumableitems_node.size() ; ++i ){
+                    const Json::Value consumableitem_node=consumableitems_node[i];
+                    if(consumableitem_node["m_itemID"] == id){
+                        m_id = consumableitem_node["m_itemID"].asInt();
+                        m_name = consumableitem_node["m_name"].asString();
+                        m_description = consumableitem_node["m_description"].asString();
+                        m_sellvalue = consumableitem_node["m_sellvalue"].asInt();
+                        m_buyvalue = consumableitem_node["m_buyvalue"].asInt();
+                        m_healthbonus = consumableitem_node["m_healthbonus"].asInt();
+                    }
+                }
+            }else{return ;}
+        }
+        int getHealthBonus(){return m_healthbonus;}
     protected:
     private:
         int m_healthbonus;
