dcc995a3d767a85c19c6735f2968cc1d33703995
Jordu29 <j.hoareau2908@gmail.com>
2016-03-18 17:18:59 +0100

    IsometricMapNode - created

---
+++ include/GameEngine/Maps/IsometricMapNode.h
@@ -0,0 +1,29 @@
+#ifndef ISOMETRICMAPNODE_H
+#define ISOMETRICMAPNODE_H
+
+
+class IsometricMapNode
+{
+    public:
+    IsometricMapNode();
+    ~IsometricMapNode();
+
+    void drawFloors(sf::RenderWindow *win, sf::Color &color);
+    void drawObjects(sf::RenderWindow *win, sf::Color &color);
+    void drawWalls(sf::RenderWindow *win, sf::Color &color);
+    void drawRoofs(sf::RenderWindow *win, sf::Color &color);
+
+    void addFloor(IsometricMapSprite *s);
+    void addWall(IsometricMapSprite *s);
+    void addRoof(IsometricMapSprite *s);
+
+    void insertSprite(IsometricMapSprite *s);
+    void removeSprite(IsometricMapSprite *s);
+    void clear();
+
+
+    protected:
+
+    std::list<IsometricMapSprite *> m_floors, m_walls, m_roofs, m_objects;
+};
+#endif // ISOMETRICMAPNODE_H
---
+++ src/GameEngine/Maps/IsometricMapNode.cpp
@@ -0,0 +1,11 @@
+#include "IsometricMapNode.h"
+
+IsometricMapNode::IsometricMapNode()
+{
+    //ctor
+}
+
+IsometricMapNode::~IsometricMapNode()
+{
+    //dtor
+}
