eef9bf11a412b93eebff380bbbb92f81104b1751
Jordan HOAREAU <j.hoareau2908@gmail.com>
2016-03-23 20:16:59 +0100

    Maps - updated
    # maps
    - removed conflict

--- src/World/Map.cpp
+++ src/World/Map.cpp
@@ -16,52 +16,6 @@ void Map::draw(sf::RenderTarget& target, sf::RenderStates states) const
 
 
 int Map::load(int id){
-
-/*
-<<<<<<< HEAD
-    //  DEV : TileMap filled with 0
-    int tiles[MapsConstant::MAPSIZE];
-    for(int i=0 ; i < MapsConstant::MAPSIZE ; i++) tiles[i] = 0;
-    if (!m_tileset.loadFromFile((string) "data\\Tiles\\tile.png"))
-        return false;
-
-    ///////////////////////////////////////
-    //  FLOOR LOADING
-
-    // Vertices : set shape to Quads and number of m_ground = numberoftiles*4 (4 m_ground/Quads)
-    m_floor.setPrimitiveType(sf::Quads);
-    m_floor.resize(MapsConstant::MAPSIZE * 4);
-
-    for (unsigned int i = 0; i < MapsConstant::HEIGHT; ++i)
-        for (unsigned int j = 0; j < MapsConstant::WIDTH; ++j)
-        {
-            // Get current tileNumber
-            int tileNumber = tiles[j + i * MapsConstant::WIDTH];
-
-            // Tile position to get in Texture
-            int tu = (tileNumber % MapsConstant::TILESET_WIDTH) * MapsConstant::TILE_WIDTH;
-            int tv = (tileNumber/MapsConstant::TILESET_WIDTH);
-
-            // Tile position to set on screen
-            int x = j*MapsConstant::TILE_WIDTH/2;
-            int y = (i * MapsConstant::TILE_HEIGHT/2) + (MapsConstant::TILE_HEIGHT/4)*(j%2);
-
-            // Get effective m_floor[4]
-            sf::Vertex* quad = &m_floor[(j + i * MapsConstant::WIDTH) * 4];
-
-            // 4 corners - Screen
-            quad[0].position = sf::Vector2f(x,y);
-            quad[1].position = sf::Vector2f(x + MapsConstant::TILE_WIDTH, y );
-            quad[2].position = sf::Vector2f(x + MapsConstant::TILE_WIDTH, y + MapsConstant::TILE_HEIGHT );
-            quad[3].position = sf::Vector2f(x, y + MapsConstant::TILE_HEIGHT );
-
-            // 4 corners - Texture
-            quad[0].texCoords = sf::Vector2f(tu, tv);
-            quad[1].texCoords = sf::Vector2f(tu+MapsConstant::TILE_WIDTH, tv);
-            quad[2].texCoords = sf::Vector2f(tu+MapsConstant::TILE_WIDTH, tv+MapsConstant::TILE_HEIGHT);
-            quad[3].texCoords = sf::Vector2f(tu, tv+MapsConstant::TILE_HEIGHT);
-            */
-//=======
             Json::Value root;
             Json::Reader reader;
             std::ifstream maps_file("data\\Maps\\Maps.json", std::ifstream::binary);
