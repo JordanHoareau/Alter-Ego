94175e704bde7a2695c8e0f0952587f4d9770174
Kyanto <tiiko.kyanto@hotmail.fr>
2016-03-24 12:56:40 +0100

    MapEvent - updated
    new constructors
    some getters/setters

--- include/GameEngine/MapEvent.h
+++ include/GameEngine/MapEvent.h
@@ -1,6 +1,7 @@
 #ifndef MAPEVENT_H
 #define MAPEVENT_H
 
+#include <array>
 #include <iostream>
 using namespace std;
 
@@ -8,16 +9,20 @@ class MapEvent
 {
     public:
         MapEvent(){}
-        MapEvent(int flag, int x, int y);
+        MapEvent(int flag, int x, int y);   //  Constructor moveEvent
+        MapEvent(int flag, int id);         //  Constructor idEvent
         virtual ~MapEvent();
 
-        void print(){
-            cout << "Je suis un map event, type : " << m_flagReturn << endl;
+        int getFlag(){
+            return m_flagReturn;
+        }
+        std::array<int,2> getDestination(){
+            return m_posEvent;
         }
     private:
         int m_flagReturn;
         int m_idReturn;
-        int m_posEvent[2];
+        std::array<int,2> m_posEvent;
 };
 
 #endif // MAPEVENT_H
--- src/GameEngine/MapEvent.cpp
+++ src/GameEngine/MapEvent.cpp
@@ -6,6 +6,12 @@ MapEvent::MapEvent(int flag, int x, int y){
     m_posEvent[1] = y;
 }
 
+MapEvent::MapEvent(int flag, int id){
+    m_flagReturn = flag;
+    m_idReturn = id;
+
+}
+
 MapEvent::~MapEvent()
 {
     //dtor
